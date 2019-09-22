252bbab7d81d3af2104244d6e8e0035e6bad5a73
fellipe Premazzi . <felliperego@hotmail.com>
2019-09-22 19:08:43 -0300

    tipo de relatorio

--- src/JFrames/jdRelatorioOrc.java
+++ src/JFrames/jdRelatorioOrc.java
@@ -74,7 +74,7 @@ public class jdRelatorioOrc extends javax.swing.JFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Relatorio"));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Pedido", "Recibo" }));
 
         jLabel1.setText("Tipo");
 
